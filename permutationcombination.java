06c10dac2de601e82ccbafb4573f15758efec2f2
SREEYA BADAM <111269199+sreeyabadam@users.noreply.github.com>
2022-08-20 16:44:23 +0530

    Create permutationcombination.java

---
+++ permutationcombination.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+class Main{
+    static int fact(int n){
+        int fact =1;
+        for (int i=1;i<=n;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n value: ");
+        int n=sc.nextInt();
+        System.out.println("Enter r value: ");
+        int r=sc.nextInt();
+        try{
+            int ncr=fact(n)/(fact(n-r)*fact(r));
+            int npr=fact(n)/fact(n-r);
+            System.out.println("Permutation: "+npr+"\n"+"Combination: "+ncr);
+        }
+        catch (ArithmeticException e){
+            System.out.println("There is an exception raised");
+        }
+        
+    }
+}
